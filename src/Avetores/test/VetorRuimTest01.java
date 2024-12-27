5810a0a457697bdec80934faec1149491c6d42c0
Caio Filardis <caiosantosshake@hotmail.com>
2024-12-27 19:07:32 -0300

    Revisando conceito de vetores, melhorando o algoritmo

---
+++ src/Avetores/test/VetorRuimTest01.java
@@ -0,0 +1,13 @@
+package Avetores.test;
+
+import Avetores.dominio.VetorRuim;
+
+public class VetorRuimTest01 {
+
+    public static void main(String[] args) {
+        VetorRuim vetorRuim = new VetorRuim(5);
+
+        vetorRuim.adicionar01("Caio");
+        vetorRuim.adicionar01("Santos");
+    }
+}
